79287fcaf746fc3637499f6b68dbc1e7d9e8618b
sanin <saninthottungalhere@gmail.con>
2024-05-23 16:51:31 +0530

    added from/to map in model

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -10,10 +10,29 @@ class TodoModel {
     this.isCompleted = false,
   }) : uid = const Uuid().v4();
 
+  //copyWith
+
   TodoModel copyWith({String? task, bool? isCompleted}) {
     return TodoModel(
       task: task ?? this.task,
       isCompleted: isCompleted ?? this.isCompleted,
     );
+  }
+
+  //fromMap
+
+  factory TodoModel.fromMap(Map<String, dynamic> map) {
+    return TodoModel(
+      task: map['task'],
+      isCompleted: map['isCompleted'],
+    );
+  }
+
+  //toMap
+  Map<String, dynamic> toMap() {
+    return {
+      'task': task,
+      'isCompleted': isCompleted,
+    };
   }
 }
